af1ae92cebae157199ef21ec94cae28c3876fa3e
yoyo9747 <josephro9747@gmail.com>
2024-11-06 15:25:11 +0900

    removedbg

--- block/blk-core.c
+++ block/blk-core.c
@@ -885,9 +885,10 @@ void submit_bio(struct bio *bio)
 	} else if (bio_op(bio) == REQ_OP_WRITE) {
 		//printk("submit_bio - ZONE WRITE BIO!\n");
 		count_vm_events(PGPGOUT, bio_sectors(bio));
-	} else if (bio_op(bio) == REQ_OP_ZONE_APPEND) {
-		printk("submit_bio - ZONE APPEND BIO!\n");
-	}
+	} 
+//else if (bio_op(bio) == REQ_OP_ZONE_APPEND) {
+//		printk("submit_bio - ZONE APPEND BIO!\n");
+//	}
 
 	bio_set_ioprio(bio);
 	submit_bio_noacct(bio);
