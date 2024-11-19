e0a3cf1af725d462d31602d855c192efbc72b8d4
yoyo9747 <josephro9747@gmail.com>
2024-11-19 12:06:30 +0900

    newverisonZA2

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -3809,10 +3809,7 @@ void f2fs_outplace_write_data(struct dnode_of_data *dn,
 	if (fio->io_type == FS_DATA_IO || fio->io_type == FS_CP_DATA_IO)
 		f2fs_update_age_extent_cache(dn);
 	set_summary(&sum, dn->nid, dn->ofs_in_node, fio->version);
-	if (PAGE_TYPE_ON_MAIN(fio.type))
-		do_append_page(&sum,fio);
-	else
-		do_write_page(&sum, fio);
+	do_write_page(&sum, fio);
 	f2fs_update_data_blkaddr(dn, fio->new_blkaddr);
 	f2fs_update_iostat(sbi, dn->inode, fio->io_type, F2FS_BLKSIZE);
 }
